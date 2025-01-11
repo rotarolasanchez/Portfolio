e1c5f3fe4fd9e0218b79db094a5ca6e3428eaf56
vistony <desarrollovistony@gmail.com>
2025-01-11 18:59:21 -0500

    avance

--- app/src/test/java/com/rotarola/portafolio_kotlin/presentation/view/atoms/TextKtTest.java
+++ app/src/test/java/com/rotarola/portafolio_kotlin/presentation/view/atoms/TextKtTest.java
@@ -6,20 +6,6 @@ import org.junit.Test;
 
 public class TextKtTest {
 
-    @Test
-    public void testTextM3() throws Exception {
-        TextKt.TextM3(0, true, "text", "placeholder", "label", null, 0, true, 0, 0L, "textDownEditext");
-    }
-
-    @Test
-    public void testEditextM3() throws Exception {
-        TextKt.EditextM3(0, true, "value", "placeholder", "label", true, null, 0, true, 0, true, 0d, null, "textDownEditext", true, null, true, true, null, null);
-    }
-
-    @Test
-    public void testSimpleText() throws Exception {
-        TextKt.SimpleText("text", 0);
-    }
 }
 
 //Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
