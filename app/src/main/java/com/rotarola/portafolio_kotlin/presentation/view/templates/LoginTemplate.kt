371539769bf777173b3febf5dcb73e4232b04b90
vistony <desarrollovistony@gmail.com>
2025-01-10 20:23:30 -0500

    avance

--- app/src/main/java/com/rotarola/portafolio_kotlin/presentation/view/templates/LoginTemplate.kt
+++ app/src/main/java/com/rotarola/portafolio_kotlin/presentation/view/templates/LoginTemplate.kt
@@ -56,7 +56,7 @@ fun LoginTemplate(
     loginViewModel: LoginViewModel = hiltViewModel(),
     onLoginSuccess: () -> Unit
 ) {
-    Log.e("LoginTemplate____", "LoginTemplate")
+    Log.e("LoginTemplate_____", "LoginTemplate")
     val userCode = loginViewModel.userCode.collectAsState()
     val userPassword = loginViewModel.userPassword.collectAsState()
     val isSnackBackBarSucessful = loginViewModel.isSnackBackBarSucessful.collectAsState()
