00ab008e8119962a835bdaedd930e973e79c258a
vistony <desarrollovistony@gmail.com>
2025-04-18 18:01:58 -0500

    test sonarqube self-hosted

--- app/src/main/java/com/rotarola/portafolio_kotlin/presentation/view/pages/AppPage.kt
+++ app/src/main/java/com/rotarola/portafolio_kotlin/presentation/view/pages/AppPage.kt
@@ -7,7 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationMain() {
     val navController = rememberNavController()
