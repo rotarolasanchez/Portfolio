230b23f515328277bc29b123c51f8027b72bc9f2
vistony <desarrollovistony@gmail.com>
2025-04-19 18:38:24 -0500

    test sonarqube self-hosted 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,7 +107,8 @@ android {
             property("sonar.projectKey", "Portfolio_kotlin")
             property("sonar.organization", "")
             property("sonar.host.url", "https://76a7-38-43-111-130.ngrok-free.app")
-            property("sonar.token", "github-token-sonar-self-hosted")
+            //property("sonar.token", "github-token-sonar-self-hosted")
+            property("sonar.token", System.getenv("github-token-sonar-self-hosted"))
             property("sonar.coverage.jacoco.xmlReportPaths", "${buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
             property("sonar.junit.reportPaths", "${buildDir}/test-results/test")
         }
