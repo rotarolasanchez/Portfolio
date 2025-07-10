9db7ec43bdca0468b804503f29da7b65bc0b2d8a
vistony <desarrollovistony@gmail.com>
2025-07-09 22:41:00 -0500

    avance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,8 @@ android {
         applicationId = "com.rotarola.portafolio_kotlin"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.1.1"
+        versionCode = 7
+        versionName = "2.3.0"
         multiDexEnabled = true
         //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //testInstrumentationRunner = "dagger.hilt.android.testing.HiltTestRunner"
